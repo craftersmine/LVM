84ccbd3e6fce9224f07cbd14984c4d6ddb955358
craftersmine <rodion.morozov.01@gmail.com>
2021-02-02 18:42:07 +0700

    Added IDeviceConfigurator Interface

---
+++ craftersmine.LVM.Core/IDeviceConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace craftersmine.LVM.Core
+{
+    public interface IDeviceConfigurator
+    {
+        public bool AutoWidth { get; set; }
+        public bool AutoHeight { get; set; }
+
+        public void ApplyConfig(IDevice device);
+
+        public void LoadConfig(IDevice device);
+    }
+}
