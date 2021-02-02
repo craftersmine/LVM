e70c9e5abb69cacd1ec361d1d69007c9bd9d9732
craftersmine <rodion.morozov.01@gmail.com>
2021-02-02 18:41:40 +0700

    Added Device Configurator Exception

---
+++ craftersmine.LVM.Core/Exceptions/DeviceConfiguratorException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.LVM.Core.Exceptions
+{
+
+    [Serializable]
+    public class DeviceConfiguratorException : Exception
+    {
+        public DeviceConfiguratorException() { }
+        public DeviceConfiguratorException(string message) : base(message) { }
+        public DeviceConfiguratorException(string message, Exception inner) : base(message, inner) { }
+        protected DeviceConfiguratorException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
