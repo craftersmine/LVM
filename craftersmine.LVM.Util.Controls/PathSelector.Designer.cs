5023bd3e70a8dd2f31581c35d00bcbc44d7050f4
craftersmine <rodion.morozov.01@gmail.com>
2021-02-02 18:39:32 +0700

    Added utils controls library

---
+++ craftersmine.LVM.Util.Controls/PathSelector.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace craftersmine.LVM.Util.Controls
+{
+    partial class PathSelector
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.path = new System.Windows.Forms.TextBox();
+            this.select = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // path
+            // 
+            this.path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.path.Location = new System.Drawing.Point(3, 3);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(248, 20);
+            this.path.TabIndex = 0;
+            // 
+            // select
+            // 
+            this.select.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.select.Location = new System.Drawing.Point(257, 1);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(26, 23);
+            this.select.TabIndex = 1;
+            this.select.Text = "...";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // PathSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.path);
+            this.Name = "PathSelector";
+            this.Size = new System.Drawing.Size(286, 26);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button select;
+    }
+}
---
+++ craftersmine.LVM.Util.Controls/PathSelector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace craftersmine.LVM.Util.Controls
+{
+    public partial class PathSelector : UserControl
+    {
+        public SelectorType SelectorType { get; set; }
+        public string FileFilter { get; set; }
+        public string DialogTitle { get; set; }
+        public string SelectedPath { get { return path.Text; } set { path.Text = value; } }
+
+        public PathSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+            switch (SelectorType)
+            {
+                case SelectorType.File:
+                    using (OpenFileDialog dlg = new OpenFileDialog())
+                    {
+                        dlg.Filter = FileFilter;
+                        dlg.Title = DialogTitle;
+                        switch (dlg.ShowDialog())
+                        {
+                            case DialogResult.OK:
+                                SelectedPath = dlg.FileName;
+                                path.Text = SelectedPath;
+                                break;
+                        }
+                    }
+                    break;
+                case SelectorType.Folder:
+                    using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+                    {
+                        dlg.Description = DialogTitle;
+                        switch (dlg.ShowDialog())
+                        {
+                            case DialogResult.OK:
+                                SelectedPath = dlg.SelectedPath;
+                                path.Text = SelectedPath;
+                                break;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    public enum SelectorType 
+    {
+        Folder,
+        File
+    }
+}
